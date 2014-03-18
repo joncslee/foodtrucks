2e22b57271ed6211f27f5996729407403992e893
Jonathan Lee <joncslee@gmail.com>
2014-03-18 18:44:47 -0400

    search form renders dummy data via ajax

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,4 +1,10 @@
 class TrucksController < ApplicationController
   def search
+    @trucks = TruckPost.all
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Foodtrucks::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'trucks#search'
+  post '/search' => 'trucks#search'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

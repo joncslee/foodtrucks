b8f64e14bd10522773ca3dc2fdcb5a269d31ec3c
Jonathan Lee <joncslee@gmail.com>
2014-03-18 20:42:31 -0400

    basic search functionality based on day, time frame, and distance from location

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,6 +1,14 @@
 class TrucksController < ApplicationController
   def search
-    @trucks = TruckPost.all
+    @address = Rails.env.production? ? request.location.address : nil
+
+    if request.xhr?
+      # perform search for today
+      # geo query: near(address, radius)
+      @trucks = TruckPost.where('day_of_week = ?', Date.today.cwday).
+        within_time_frame(params[:time_frame]).
+        near(params[:address], 2)
+    end
 
     respond_to do |format|
       format.html
--- app/models/truck_post.rb
+++ app/models/truck_post.rb
@@ -1,4 +1,16 @@
 class TruckPost < ActiveRecord::Base
+  reverse_geocoded_by :latitude, :longitude
   belongs_to :brand
   validates_presence_of :day_of_week, :start_time, :end_time
+
+  def self.within_time_frame(time_frame)
+    case time_frame
+    when "breakfast"
+      where("start_time BETWEEN '05:00:01' AND '10:59:59'")
+    when "lunch"
+      where("start_time BETWEEN '11:00:01' AND '15:59:59'")
+    when "dinner"
+      where("start_time BETWEEN '16:00:01' AND '20:59:59'")
+    end
+  end
 end
